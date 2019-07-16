fda406b5872e135ae0296fece03440179e5d36df
wsadrak <w.sadrak@gmail.com>
2019-07-16 13:34:06 +0200

    added constructor for creating Task

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,22 @@
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Task {
+
 	String description;
 	Priority priority;
-	Date date;
+	LocalDate date;
+	
+	Task(String description, Priority priority, LocalDate date) {
+		this.description = description;
+		this.priority = priority;
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [description=" + description + ", priority=" + priority + ", date=" + date + "]";
+	}
+	
 	
 	
 }
