d33cb89f7235f9614a2c0ffe96bd2ebfc8092604
wsadrak <w.sadrak@gmail.com>
2019-07-17 15:23:17 +0200

    created new exception class for data import/export

---
+++ src/exception/DataExportException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class DataExportException extends RuntimeException {
+	private static final long serialVersionUID = 8180028095224327003L;
+
+	public DataExportException(String message) {
+		super(message);
+	}
+}
---
+++ src/exception/DataImportException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class DataImportException extends RuntimeException {
+
+	private static final long serialVersionUID = -4291962429921075928L;
+
+	public DataImportException(String message) {
+		super(message);
+	}
+}
