95d0a7436de86149909d0e1d3080050b557eff9a
wsadrak <w.sadrak@gmail.com>
2019-07-17 15:22:29 +0200

    added new method for printing tasks

--- src/io/ConsolePrinter.java
+++ src/io/ConsolePrinter.java
@@ -1,14 +1,23 @@
 package io;
 
+import java.util.List;
+
 import model.Database;
+import model.Task;
 
 public class ConsolePrinter {
 	public void printLine(String text) {
 		System.out.println(text);
 	}
 
-	public void printTasks(Database database) {
-		// TODO Auto-generated method stub
-		
+//	public void printTasks(Object sortedByPriority) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+
+	public void printTasks(List<Task> database) {
+		for(Task anotherTask : database) {
+			printLine(anotherTask.toString());
+		}
 	}
 }
