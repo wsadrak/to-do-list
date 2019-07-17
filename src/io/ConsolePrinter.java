f533e888dbc95979d556111192ea747ed70ec4ea
wsadrak <w.sadrak@gmail.com>
2019-07-17 11:33:58 +0200

    created class for printing output in console

---
+++ src/io/ConsolePrinter.java
@@ -0,0 +1,14 @@
+package io;
+
+import model.Database;
+
+public class ConsolePrinter {
+	public void printLine(String text) {
+		System.out.println(text);
+	}
+
+	public void printTasks(Database database) {
+		// TODO Auto-generated method stub
+		
+	}
+}
