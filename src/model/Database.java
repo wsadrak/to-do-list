46568286c94db6655047288769fd6b6ca1eaf26e
wsadrak <w.sadrak@gmail.com>
2019-07-17 11:38:04 +0200

    creating new Task moved to another class

--- src/model/Database.java
+++ src/model/Database.java
@@ -1,47 +1,17 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+package model;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListController {
+public class Database {
 	private List<Task> dataBase = new ArrayList<>();
-	DataReader dataReader = new DataReader();
-	public void addNewTask() {
 
-		String description = dataReader.readInput();
-		Priority priority = readPriority();
-		LocalDate date = readDate();
-		
-		dataBase.add(new Task(description, priority, date));
+	public void addTask(Task task) {
+		dataBase.add(task);
 	}
-	private LocalDate readDate() {
-		System.out.println("Podaj datê w formacie dd-MM-yyyy");
-		DateTimeFormatter datePattern = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		String dateInput = dataReader.readInput();
-		LocalDate createdDate = LocalDate.parse(dateInput, datePattern);
-		return createdDate;
-	}
-	private Priority readPriority() {
-		System.out.println("Type priority: ");
-		String priodesc = dataReader.readInput();
-		
-		if(priodesc.equalsIgnoreCase("medium")) {
-			return Priority.MEDIUM;
-		} else if(priodesc.equalsIgnoreCase("low")) {
-			return Priority.LOW;
-		} else if(priodesc.equalsIgnoreCase("high")) {
-			return Priority.HIGH;
-		}
-		return Priority.EMPTY;
-		
-		// TODO - obs³u¿yæ sytuacjê gdy u¿ytkownik wpisze coœ innego
-			
-			
-		
-	}
+
 	public void showTasks() {
-//		dataBase.
-		for(Task tasks: dataBase) {
+		for (Task tasks : dataBase) {
 			System.out.println(tasks);
 		}
 	}
