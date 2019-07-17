acae48ec09ef999a0c23b969bc57c883ae3af5a2
wsadrak <w.sadrak@gmail.com>
2019-07-17 15:22:11 +0200

    added file service

--- src/application/ApplicationController.java
+++ src/application/ApplicationController.java
@@ -1,6 +1,10 @@
 package application;
+
+import exception.DataExportException;
+import exception.DataImportException;
 import io.ConsolePrinter;
 import io.DataReader;
+import io.file.FileManager;
 import model.Database;
 import model.Option;
 import model.Task;
@@ -8,9 +12,22 @@ import model.Task;
 public class ApplicationController {
 	private ConsolePrinter printer = new ConsolePrinter();
 	private DataReader dataReader = new DataReader(printer);
-	
-	private Database database = new Database();
-	
+
+	private Database database;
+	private FileManager fileManager = new FileManager();
+
+	public ApplicationController() {
+		try {
+			database = fileManager.importData();
+			System.out.println("Zaimportowano dane z pliku");
+
+		} catch (DataImportException e) {
+			printer.printLine(e.getMessage());
+			printer.printLine("Zainicjowano now¹ bazê.");
+			database = new Database();
+		}
+	}
+
 	public void mainLoop() {
 		Option option;
 		do {
@@ -38,7 +55,7 @@ public class ApplicationController {
 	}
 
 	private void addTask() {
-		
+
 		Task newTask = dataReader.readAndCreateTask();
 		database.addTask(newTask);
 	}
@@ -54,19 +71,69 @@ public class ApplicationController {
 	}
 
 	private void showTasks() {
-		database.showTasks();
-		printer.printTasks(database);
+
+		printer.printLine("Jak chcesz posortowaæ?: ");
+
+		printSubMenu();
+		int optionValue = dataReader.getInt();
+
+		switch (optionValue) {
+		case 0:
+			printDefault();
+			break;
+		case 1:
+			printByPriority();
+			break;
+		case 2:
+			printByDate();
+			break;
+		default:
+			printDefault();
+
+		}
+
+//		database.showTasks();
+//		printer.printTasks(database.getSortedTasks());
+	}
+
+	private void printDefault() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void printByDate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void printByPriority() {
+		// TODO Auto-generated method stub
+		printer.printTasks(database.getSortedByPriority());
+
+	}
+
+	private void printSubMenu() {
+		System.out.println("[0] - po id ");
+		System.out.println("[1] - po priorytecie");
+		System.out.println("[2] - po dacie");
 	}
 
 	private void exit() {
-		System.out.println("exit");
-	}
 
+		try {
+			fileManager.exportData(database);
+			printer.printLine("Eksport danych do pliku zakoñczony powodzeniem");
+		} catch (DataExportException e) {
+			printer.printLine(e.getMessage());
+		}
+		dataReader.close();
+		printer.printLine("Koniec programu, papa");
+	}
 
 	private Option getOption() {
 		Option option = null;
 		boolean isOptionCorrect = false;
-		while(!isOptionCorrect) {
+		while (!isOptionCorrect) {
 			option = Option.createFromInt(dataReader.getInt());
 			isOptionCorrect = true;
 		}
