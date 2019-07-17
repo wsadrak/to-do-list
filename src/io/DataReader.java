d1462372c20a6c85e666aedb061b9330ba29eac3
wsadrak <w.sadrak@gmail.com>
2019-07-17 11:37:41 +0200

    method for reading input and creating task moved to this class

--- src/io/DataReader.java
+++ src/io/DataReader.java
@@ -1,24 +1,65 @@
+package io;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import model.Task;
+import model.Priority;
 
 public class DataReader {
-
 	private Scanner scanner = new Scanner(System.in);
+	private ConsolePrinter printer;
+
+	public DataReader(ConsolePrinter printer) {
+		this.printer = printer;
+	}
 
 	public int getInt() {
 		try {
-			return Integer.parseInt(readInput());
+			return Integer.parseInt(readText());
 		} catch (NumberFormatException e) {
 			throw new NoSuchElementException("It's not a number");
 		}
 	}
 
-	public String readInput() {
-		return scanner.nextLine();
-	}
-	
 	public void close() {
 		scanner.close();
+	}
+
+	public Task readAndCreateTask() {
+		printer.printLine("Podaj teœæ zadania: ");
+		String description = readText();
+		printer.printLine("Podaj priorytet: ");
+		Priority priority = readPriority();
+		printer.printLine("Podaj datê w formacie dd-MM-yyyy: ");
+		LocalDate date = readDate();
+		return new Task(description, priority, date);
+	}
+
+	public String readText() {
+		return scanner.nextLine();
+	}
+
+	private Priority readPriority() {
+		String priority = readText();
+		if (priority.equalsIgnoreCase("low")) {
+			return Priority.LOW;
+		} else if (priority.equalsIgnoreCase("medium")) {
+			return Priority.MEDIUM;
+		} else if (priority.equalsIgnoreCase("high")) {
+			return Priority.HIGH;
+		} else {
+			printer.printLine("Nie wprowadzono poprawnej wartoœci. Ustawiono wartoœæ domyœln¹ LOW");
+		}
+		return Priority.LOW;
+	}
+
+	private LocalDate readDate() {
+		DateTimeFormatter datePattern = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		String dateInput = readText();
+		LocalDate createdDate = LocalDate.parse(dateInput, datePattern);
+		return createdDate;
 	}
 
 }
