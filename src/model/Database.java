4a89cc9472d733a3e5a9d2eea0cada9dca757214
wsadrak <w.sadrak@gmail.com>
2019-07-19 16:34:23 +0200

    added default soring method and default sorting method, added method for setting id

--- src/model/Database.java
+++ src/model/Database.java
@@ -2,17 +2,25 @@ package model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
 
 public class Database implements Serializable {
 	private static final long serialVersionUID = 7908549133902311217L;
 	private List<Task> listOfTasks = new ArrayList<>();
 
 	public void addTask(Task task) {
+		int biggestId = 1000;
+
+		for (Task singleTask : listOfTasks) {
+			if (singleTask.getTaskId() >= biggestId) {
+				biggestId = singleTask.getTaskId() + 1;
+			}
+		}
+
+		task.setTaskId(biggestId);
 		listOfTasks.add(task);
 	}
 
@@ -21,20 +29,41 @@ public class Database implements Serializable {
 			System.out.println(tasks);
 		}
 	}
-	
+
 	// TODO - poprawiæ
 	public List<Task> getSortedByPriority() {
 		Comparator<Task> comp = new Comparator<Task>() {
 			@Override
 			public int compare(Task o1, Task o2) {
-				return o1.getPriority().compareTo(o2.getPriority());
+				return -o1.getPriority().compareTo(o2.getPriority());
 			}
 		};
-		Collections.sort(listOfTasks, comp );
-		
+		Collections.sort(listOfTasks, comp);
+
 		return listOfTasks;
 	}
 
-	
+	public List<Task> getSortedByDate() {
+		Comparator<Task> comp = new Comparator<Task>() {
+			@Override
+			public int compare(Task o1, Task o2) {
+				return o1.getDate().compareTo(o2.getDate());
+			}
+		};
+		Collections.sort(listOfTasks, comp);
+
+		return listOfTasks;
+	}
+
+	public List<Task> getSortedDefault() {
+		Comparator<Task> comp = new Comparator<Task>() {
+			@Override
+			public int compare(Task o1, Task o2) {
+				return Integer.compare(o1.getTaskId(), o2.getTaskId());
+			}
+		};
+		Collections.sort(listOfTasks, comp);
+		return listOfTasks;
+	}
 
 }
