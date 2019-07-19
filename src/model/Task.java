1bb20b267592ffd634f0b578c4ea07aff4827585
wsadrak <w.sadrak@gmail.com>
2019-07-19 16:32:41 +0200

    refactoring of toString() method, removed compareTo() method, added taskId field

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,20 +1,22 @@
 package model;
 
+import java.io.PrintStream;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Task implements Comparable<Task>, Serializable {
+public class Task implements Serializable {
 	private static final long serialVersionUID = 2413167338316421989L;
 	String description;
 	Priority priority;
 	LocalDate date;
+	private int taskId = 1000;
 
 	public Task(String description, Priority priority, LocalDate date) {
 		this.description = description;
 		this.priority = priority;
 		this.date = date;
+		this.setTaskId(1000);
 	}
-
 	public String getDescription() {
 		return description;
 	}
@@ -39,22 +41,43 @@ public class Task implements Comparable<Task>, Serializable {
 		this.date = date;
 	}
 
+	
 	@Override
 	public String toString() {
-		return description + " | " + date + " | " + priority;
+		return String.format("%-5s| %20s     | %8s     | %12s |", taskId, description, priority, date);
 	}
 
-	@Override
-	public int compareTo(Task t) {
-		int priorityCompare = priority.compareTo(t.getPriority());
-	    if (priorityCompare != 0) {
-	        return priorityCompare;
-	    }
-	    int dateCompare = date.compareTo(t.getDate());
-	    if (dateCompare != 0) {
-	        return dateCompare;
-	    }
-	    return description.compareTo(t.getDescription());
+//	@Override
+//	public int compareTo(Task t) {
+//		
+//		public int compare(Task o1, Task o2) {
+//			return Integer.compare(o1.getTaskId(), o2.getTaskId());
+//		}
+//		
+//		int idCompare = Integer.taskId.compareTo(t.getPriority());
+//	    if (priorityCompare != 0) {
+//	        return priorityCompare;
+//	    }
+//	    
+//		int priorityCompare = priority.compareTo(t.getPriority());
+//	    if (priorityCompare != 0) {
+//	        return priorityCompare;
+//	    }
+//	    int dateCompare = date.compareTo(t.getDate());
+//	    if (dateCompare != 0) {
+//	        return dateCompare;
+//	    }
+//	    return description.compareTo(t.getDescription());
+////	    return 
+//	}
+
+	public int getTaskId() {
+		return taskId;
 	}
 
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	
 }
